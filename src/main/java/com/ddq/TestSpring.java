7a535ee7deae5e4439681cacb7a93393c8ce75a4
dongqingdai <dongqingdai@creditease.cn>
2019-11-08 17:57:31 +0800

    spring框架学习项目，此次版本中，添加了一点关于注解的内容： 通过 @Value 注解获取 *.properties 配置文件中的值

--- src/main/java/com/ddq/TestSpring.java
+++ src/main/java/com/ddq/TestSpring.java
@@ -5,13 +5,35 @@ import com.ddq.pojo.Source;
 import com.ddq.service.HelloService;
 import com.ddq.service.ProductService;
 import org.junit.Test;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class TestSpring {
 
+
+//    @Bean
+//    public static PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer() {
+//        return new PropertySourcesPlaceholderConfigurer();
+//    }
+
+    /*
+        关于程序启动的一些理解：
+        所有的程序想要运行，就必须有一个入口，通常是一个main（）函数，
+        java程序的入口也是main（）函数，java虚拟机运行时，找的这个程序入口就是main（）方法，
+        在此spring项目中，我们并没有显式的定义这个入口函数main()方法，
+        但是，我们依然能够正常的启动程序，并且运行项目，这是因为，我们使用了Junit测试框架，
+        当我们没有显式的定义main方法时，我们是通过在要执行的方法上加上了@Test注解，
+        通过这个@Test注解，就声明了这个方法是一个测试方法，Junit中自身定义了一个main()函数作为程序的入口，
+        当我们将一个方法或者类通过Junit的注解声明为一个测试方法或者测试类时，
+        运行这个测试方法或者测试类时，使用的程序入口就是Junit中自身定义的main方法。
+        所以，如果不给方法添加@Test注解，就必须显式的定义一个main()方法，在这个显式的main()方法中调用我们想要执行的测试方法。
+    */
+
     @Test
     public void test01(){
+
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
         /*
             控制反转：创建对象交由spring框架来完成  Inverse of Control（控制反转）
@@ -19,7 +41,50 @@ public class TestSpring {
             正控：若要使用某个对象，需要自己去负责对象的创建
             反控：若要使用某个对象，只需要从 Spring 容器中获取需要使用的对象，不关心对象的创建过程，也就是把创建对象的控制权反转给了Spring框架
             好莱坞法则：Don’t call me ,I’ll call you
+
             以上创建ClassPathXmlApplicationContext对象的语句就是创建IOC容器的，IOC容器是创建，管理bean的。
+            ClassPathXmlApplicationContext用于加载CLASSPATH下的Spring配置文件，此步骤中就已经完成了对所有Bean实例的加载，
+
+            Spring的启动过程，就是其IoC容器的启动过程，本质就是创建和初始化bean的工厂（BeanFactory），
+            BeanFactory其实就是整个SpringIoc的核心，Spring 使用 BeanFactory 来实例化、配置和管理 Bean。
+
+            对于web程序，IoC容器启动过程即是建立上下文的过程，在web应用中，web容器会提供一个全局的ServletContext上下文环境，
+            ServletContext上下文为Spring IoC提供了一个宿主环境。
+
+            此Spring项目中，如果我们不显式的定义一个mian(）函数作为程序入口，
+            则在运行测试类和测试方法时，就是利用Junit自身定义的mian()方法作为程序入口，
+            当我们执行测试类或者方法时，虚拟机先找到Junit中定义的main()方法作为程序启动的入口，
+            然后再执行Spring框架的创建bean容器的语句，进而完成启动bean 容器，根据传入的spring框架配置文件寻找到配置文件，
+            进而根据配置文件中配置的bean的相关信息，去创建bean 对象。。。
+            spring框架的启动就是从IOC容器的启动开始的，即以下语句：
+            ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+
+            详细内容可见以下博客：
+            spring入门讲解：
+            https://www.cnblogs.com/wmyskxz/p/8820371.html
+
+            【Spring源码分析】Bean加载流程概览：
+            https://www.cnblogs.com/xrq730/p/6285358.html
+
+            https://www.jianshu.com/p/11c978c6552d
+
+            spring容器启动过程：
+            https://blog.51cto.com/12854546/1930134
+            https://blog.csdn.net/weixin_43687167/article/details/87629727
+            https://blog.csdn.net/CCCCC_SSSSS/article/details/81483889
+            https://blog.csdn.net/u011447614/article/details/82994126
+            https://www.jianshu.com/p/96ec66535007
+
+            spring Environment类：
+            https://www.jianshu.com/p/5f10192eb958
+
+            spring 中Resource接口详解：
+            https://blog.csdn.net/shuxing520/article/details/79345980
+
+            当通过ClassPathApplicationContext初始化容器时，它会根据定位加载spring.xml配置，
+            然后解析配置文件，生成Bean，注册Bean，最后我们在通过getBean获取对象，这一现象就跟IOC容器的初始化过程一样，
+            资源定位、资源加载、资源解析、生成Bean、Bean注册
+
          */
         HelloService helloService = (HelloService) context.getBean("helloService");
         /*
@@ -27,6 +92,7 @@ public class TestSpring {
             创建对象后，返回的是一个object类型的对象，此处用到了向上转型，所以，在返回对象后，还需要进行下向下转型。
          */
         helloService.hello();
+
     }
 
     @Test
@@ -36,6 +102,7 @@ public class TestSpring {
             ApplicationContext 是一个接口
             ClassPathXmlApplicationContext 是一个接口实现类（但是并不是直接实现Application接口）
             以上语句是利用了java的多态特性
+            context 就是一个IOC容器
          */
         ProductService pS = (ProductService) context.getBean("productService");
         pS.doSomeService();
--- src/main/java/com/ddq/pojo/Source.java
+++ src/main/java/com/ddq/pojo/Source.java
@@ -1,5 +1,16 @@
 package com.ddq.pojo;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+
+// @PropertySource("classpath:config.properties")
+/*
+    通过@PropertySource注解将properties配置文件中的值存储到Spring的 Environment中，
+    Environment接口提供方法去读取配置文件中的值，参数是properties文件中定义的key值
+    这个问题产生的原因是：spring在读取∗.properties文件时,默认使用的是asci码
+ */
 public class Source {
     /*
         定义一个bean，用于对桔汁进行抽象（建模）
@@ -7,6 +18,12 @@ public class Source {
      */
     private String fruit; // 类型
     private String sugar; // 糖
+    @Value("${size}")
+    /*
+     通过Spring的 <context:property-placeholder /> 标签配合@value 注解来获取到配置文件中的值（此情况下不需要再添加类上面的@PropertySource 注解）
+     注意：在使用@Value注解，不管是组合哪一个标签或注解，来获取配置文件中的值，如果配置文件中有中文，
+           则获取到的值，都有很大可能会是乱码，这个很容易导致问题，需要注意。
+      */
     private String size; // 大小杯
 
     public void setFruit(String fruit){
@@ -32,4 +49,15 @@ public class Source {
     public String getSize(){
         return size;
     }
+
+    @Bean
+    public static PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer() {
+        return new PropertySourcesPlaceholderConfigurer();
+    }
+    /*
+       如果要使用@PropertySource + @Value 来获取配置文件的值（此时不需要再在 applicationContext.xml文件中添加<context：property-holder />标签）
+       就必须要添加这个bean，
+       //要想使用@Value 用${}占位符注入属性，这个bean是必须的，这个就是占位bean,
+
+     */
 }
